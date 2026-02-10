5df415c92280911156b5e2a20b77dba45b505ea8
Cole VanLeer <cvanleer99@gmail.com>
2026-02-09 17:30:15 -0800

    Basic click SFX and credits

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,23 +1,35 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-    [SerializeField] String gameStartScene;
+    [SerializeField] string gameStartScene;
+    [SerializeField] AudioClip clickSFX;
+    AudioSource audioSource;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
-
+    // TODO: This SFX system is bugged and will have problems later
+    // Button Press SFX courtesy of Sonic SoundFX
     public void StartPressed()
     {
+        audioSource.PlayOneShot(clickSFX);
         SceneManager.LoadScene(gameStartScene);
+    }
+
+    public void SettingsPressed()
+    {
+        audioSource.PlayOneShot(clickSFX);
+        Debug.Log("Loading settings...");
     }
 
     public void QuitPressed()
     {
+        audioSource.PlayOneShot(clickSFX);
         Application.Quit();
         Debug.Log("Quitting game...");
     }
