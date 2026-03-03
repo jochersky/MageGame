6ab528c23e21f7c3158cc414b71a5894364b838d
Cole VanLeer <cvanleer99@gmail.com>
2026-03-02 22:21:58 -0800

    Integrated light spell

--- Assets/Scripts/Spells/LightSpell.cs
+++ Assets/Scripts/Spells/LightSpell.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class LightSpell : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
+    Light2D lightAura;
     float red;
     float green;
     float blue;
     float alpha;
+    [SerializeField] float startingIntensity = 5f;
     // time between fade steps
     [SerializeField] float fadeRate = 1f;
     // How much alpha to reduce by per fade step
-    [SerializeField] float fadeStep = 0.1f;
+    [SerializeField] float spriteFadeStep = 0.1f;
+    // how much intensity to reduce by per fade step
+    [SerializeField] float brightnessFadeStep = 0.3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        lightAura = GetComponentInChildren<Light2D>();
+        lightAura.intensity = startingIntensity;
         red = spriteRenderer.color.r;
         green = spriteRenderer.color.g;
         blue = spriteRenderer.color.b;
@@ -25,9 +32,9 @@ public class LightSpell : MonoBehaviour
 
     void Fade()
     {
-        Debug.Log(alpha);
-        alpha -= fadeStep;
+        alpha -= spriteFadeStep;
         spriteRenderer.color = new Color(red, green, blue, alpha);
+        lightAura.intensity -= brightnessFadeStep;
     }
 
     // Update is called once per frame
