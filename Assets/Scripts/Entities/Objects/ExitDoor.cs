448efbaf041422577f3a4366e8cbc25d1cdc1008
Cole VanLeer <cvanleer99@gmail.com>
2026-04-24 15:43:19 -0700

    Fixed Level 2 issues

--- Assets/Scripts/Entities/Objects/ExitDoor.cs
+++ Assets/Scripts/Entities/Objects/ExitDoor.cs
@@ -1,12 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ExitDoor : MonoBehaviour
 {
     public string nextLevel;
+    [SerializeField] string playerTag = "Player";
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(nextLevel);
+        if (collision.CompareTag(playerTag))
+            SceneManager.LoadScene(nextLevel);
     }
 }
