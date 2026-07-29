3dea83675f9c6972f752469bcc61cde8f39197d2
Josh Chernetsky <joshcherndev@gmail.com>
2026-07-28 21:38:21 -0700

    Bomb + Drill Bomb explosion VFX added

--- Assets/Scripts/Items/Consumables/Bomb.cs
+++ Assets/Scripts/Items/Consumables/Bomb.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 
 public class Bomb : Consumable
 {
-    [SerializeField] private Transform rbTransform;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer sprite;
     [SerializeField] private Collider2D explosionCollider;
     [SerializeField] private Collider2D hitboxCollider;
     [SerializeField] private DamageFlash damageFlash;
+    [SerializeField] private ParticleSystem explosionRadius;
+    [SerializeField] private ParticleSystem explosionParticles;
     [SerializeField, Range(0, 10)] private float explodeTime = 1;
     [SerializeField] private float flashSpeed = 1f;
     [SerializeField] TemporaryEffect explosionEffect;
 
+    private CountdownTimer _particleLifetimeTimer;
+    
     private float timer = 0f;
+    private bool _followRb = true;
     
     private void Start()
     {
         damageFlash.OnDamageFlashComplete += () => damageFlash.StartFlash();
+        
+        _particleLifetimeTimer = new CountdownTimer(explosionRadius.main.startLifetime.constantMax);
+        _particleLifetimeTimer.OnTimerStop += () => { Destroy(gameObject); };
         
         hitboxCollider.enabled = false;
         explosionCollider.enabled = false;
@@ -25,17 +34,26 @@ public class Bomb : Consumable
         damageFlash.StartFlash();
         StartCoroutine(InitiateExplode());
     }
+
+    private void Update()
+    {
+        _particleLifetimeTimer.Tick(Time.deltaTime);
+    }
     
     private void FixedUpdate()
     {
-        explosionCollider.transform.position = rbTransform.position;
-        hitboxCollider.transform.position = rbTransform.position;
+        if (!_followRb) return;
+        
+        explosionCollider.transform.position = rb.position;
+        hitboxCollider.transform.position = rb.position;
+        explosionRadius.transform.position = rb.position;
+        explosionParticles.transform.position = rb.position;
     }
 
-    private void Explode()
-    {
-        Destroy(gameObject);
-    }
+    // private void Explode()
+    // {
+    //     Destroy(gameObject);
+    // }
 
     private IEnumerator InitiateExplode()
     {
@@ -47,8 +65,22 @@ public class Bomb : Consumable
         }
         hitboxCollider.enabled = true;
         explosionCollider.enabled = true;
+        
+        sprite.enabled = false;
+        _followRb = false;
+        
+        // start particle effects
+        explosionRadius.Play();
+        explosionParticles.Play();
+        _particleLifetimeTimer.Start();
+        
         Instantiate(explosionEffect, transform.position, quaternion.identity);
+        
         yield return new WaitForSeconds(0.1f);
-        Explode();
+        
+        hitboxCollider.enabled = false;
+        explosionCollider.enabled = false;
+        
+        // Explode();
     }
 }
