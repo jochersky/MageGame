7d211296e9057d469eac043d6fdc69876103659e
Cole VanLeer <cvanleer99@gmail.com>
2026-09-13 18:17:06 -0700

    Audio sliders sync

--- Assets/Scripts/General/SceneManagement.cs
+++ Assets/Scripts/General/SceneManagement.cs
@@ -4,10 +4,15 @@ public class SceneManagement : MonoBehaviour
 {
     private AudioManager audioManager;
     [SerializeField] AudioClip levelMusic;
+    [SerializeField] AudioManager audioManagerPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         audioManager = FindFirstObjectByType<AudioManager>();
+        if (audioManager == null)
+        {
+            audioManager = Instantiate(audioManagerPrefab);
+        }
         audioManager.ChangeMusic(levelMusic);
     }
 
